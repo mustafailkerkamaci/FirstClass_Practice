8e7a3c9a6e7dcb7591b62eae80ef0961a70ca28d
Mustafa İlker Kamaci <mustafailkerkmc@gmail.com>
2025-05-29 03:12:47 +0300

    Add files via upload

---
+++ FIrstClass_Practice/FIrstClass_Practice/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIrstClass_Practice
+{
+    internal class Person
+    {
+
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public DateTime DogumTarihi { get; set; }
+
+        public void Bilgiler() 
+        {
+            Console.WriteLine("Ad: " + Ad);
+            Console.WriteLine("Soyad: " + Soyad);
+            Console.WriteLine("Doğum Tarihi: " + DogumTarihi.ToShortDateString()); // ToSohrtDateString() sadece tarihi gösterir, saat bilgisi yoktur.
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        }
+
+    }
+}
---
+++ FIrstClass_Practice/FIrstClass_Practice/Program.cs
@@ -0,0 +1,23 @@
+﻿using FIrstClass_Practice;
+
+Person ogrenci1 = new Person();
+ogrenci1.Ad = "Ali";   
+ogrenci1.Soyad = "Yılmaz";
+ogrenci1.DogumTarihi = new DateTime(2000, 5, 15);
+
+Person ogrenci2 = new Person();
+ogrenci2.Ad = "Ayşe";
+ogrenci2.Soyad = "Demir";
+ogrenci2.DogumTarihi = new DateTime(1998, 3, 22); 
+
+Person ogretmen1 = new Person();
+ogretmen1.Ad = "Mehmet";
+ogretmen1.Soyad = "Kara";
+ogretmen1.DogumTarihi = new DateTime(1985, 7, 10);
+
+Console.WriteLine("===== Ogrenci Bilgileri=====");
+ogrenci1.Bilgiler();
+ogrenci2.Bilgiler();
+
+Console.WriteLine("===== Ogretmen Bilgileri=====");
+ogretmen1.Bilgiler();
